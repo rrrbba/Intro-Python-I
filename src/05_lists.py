6d6a6107780ba47f8dcc3813c41f482cd79970fc
Rayven <rayven.burns@gmail.com>
2020-02-10 13:47:18 -0500

    hello world done

--- src/05_lists.py
+++ src/05_lists.py
@@ -8,10 +8,15 @@ y = [8, 9, 10]
 
 # Change x so that it is [1, 2, 3, 4]
 # YOUR CODE HERE
+
+# append, extend, insert
+#x.insert(3, 4)
+x.append(4)
 print(x)
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # YOUR CODE HERE
+x.extend(y)
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
